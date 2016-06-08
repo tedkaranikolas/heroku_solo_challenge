36766c94ea6ca1d949a1da0563725e8b826d1875
Ted Karanikolas <ted@hrg-nyc.com>
2016-06-08 11:05:54 -0500

    added stuff

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 //  x --- Set up a node project using npm init, create an http server and respond on port 3000 with “Hello world!”.
-//  Test the application locally in the browser.
+// x --- Test the application locally in the browser.
 // Note: make sure you use listen(process.env.PORT || 3000); in order to support heroku’s automatic port assignment.
 // Follow this tutorial to get heroku set up on your machine: https://devcenter.heroku.com/articles/getting-started-with-nodejs#introduction
 // post the assignment to both GitHub and Heroku and send these URLs to your instructors.
@@ -17,5 +17,19 @@ app.get( '/', function(req, res){
   console.log ('hello ted from base url get');
   res.writeHead(200);
   res.write('you are in the base url, ted');
+  res.end();
+});
+
+app.get( '/bilo', function(req, res){
+  console.log('hello via bilo');
+  res.write('we are now with peter...');
+  res.write('biggles to follow bilo hence');
+  res.end();
+});
+
+app.get( '/biggles', function(req, res){
+  console.log('biggles reporting for duty');
+  res.write('we are now with carlos...');
+  res.write('bilo is thither');
   res.end();
 });
